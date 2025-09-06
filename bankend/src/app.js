353708587c087a8f03adc7320af827c983e167b0
Achyut-007 <hebbarachyut@gmail.com>
2025-09-06 10:54:36 +0530

    Add files via upload

---
+++ bankend/src/app.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.use('/api/auth', require('./routes/auth'));
+app.listen(3000, () => console.log('Server running on port 3000'));
